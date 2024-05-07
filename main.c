2d6272c6553e9dc3a1ff7c434634de606268802c
Guilherme <guilhermeklosesantos@gmail.com>
2024-05-07 20:57:49 -0300

    Update sensor logic * Now all sensors are in a while true loop generating data while the main process it

--- main.c
+++ main.c
@@ -4,123 +4,90 @@
 #include <unistd.h>
 #include <time.h>
 
-#define NUM_THREADS_SENSOR 5
+#define NUM_THREADS_SENSOR 3
 #define NUM_THREADS_ATUADOR 5
-#define EMPTY -1
+#define ARRAY_LENGTH 1000
 
 pthread_t threads_sensor[NUM_THREADS_SENSOR];
 pthread_t threads_atuador[NUM_THREADS_ATUADOR];
-pthread_t sensor_creator;
 
 // feito para controlar fluxo.
-int sensor_data[NUM_THREADS_SENSOR];
+long sensor_queue[ARRAY_LENGTH];
+int sensor_queue_size = -1;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-// ++ condição de melhora: usar cond para assim que um dado for processado e settado para EMPTY já processar outro?
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 
 // atuadores
 void *atuador(void *arg) {
-  // processa os dados do sensor aqui
-  int sensor_data = *((int *)arg);
-  printf("ATUADOR THREAD with sensor data numa nova thread: %d\n", sensor_data);
-
-  pthread_exit(NULL);
-}
-
-void create_atuadores() {
-  for (long i = 0; i < NUM_THREADS_ATUADOR; i++) {
-    // cria thread que representa atuador
-    pthread_create(&threads_atuador[i], NULL, atuador, (void *)&sensor_data[i]);
+  while (1) {
+    // Para fazer
   }
 }
 
 // sensores
 void *sensor(void *arg) {
-  // seed para os random numeros dos sensores (baseado no id da thread e no tempo atual)
-  srand(time(NULL) ^ (long)arg);
-
-  // sleep time para pegar os dados
-  int sleep_time = rand() % 6;
-  sleep(sleep_time);
-
-  // random data dos sensores
-  int random_int = rand() % 1000;
-
-  pthread_mutex_lock(&mutex);
-  // salva dados desse sensor
-  sensor_data[(long)arg] = random_int;
-  pthread_mutex_unlock(&mutex);
-
-  printf("Sensor (sensor SIDE THREAD) %ld gerou: %d em %d segundos\n", (long)arg, random_int, (long)sleep_time);
-
-  pthread_exit(NULL);
-}
-
-// Esta side thread é responsável por criar sensores se algum slot de dados estão disponiveis.
-// + possível melhora (usar thread condition para assim que algum dado virar EMPTY instantaneamente aviasr o sensor para mandar mais dados)?
-void *create_sensors() {
-  printf("create Sensor\n");
-
-  long sensor_id;
   while (1) {
-    sleep(1);
+    // Gerar numero randomico para usar de sleep.
+    int sleep_time = (rand() % 5) + 1;
 
-    // reseta sensor_id para o numero correto
-    if (sensor_id >= NUM_THREADS_SENSOR) sensor_id = 0;
+    printf("thread %ld sleeping for %d\n", (long)arg, sleep_time);
 
-    // se ele estiver EMPTY pode prosseguir para simular dado de sensor, caso contrário ignora.
-    if (sensor_data[sensor_id] != EMPTY) continue;
+    sleep(sleep_time);
 
-    printf("não pulou\n");
+    // random data dos sensores.
+    int random_int = rand() % 1000;
 
-    // cria side thread para armazenar dado de sensor
-    pthread_create(&threads_sensor[sensor_id], NULL, sensor, (void *)sensor_id);
-    
-    // aumenta o id do sensor.
-    sensor_id++;
+    // Mutex para sincronizar.
+    pthread_mutex_lock(&mutex);
+
+    printf("thread %ld generated %d\n", (long)arg, random_int);
+
+    // salva dados desse sensor
+    sensor_queue[sensor_queue_size] = random_int;
+    sensor_queue_size++;
+
+    pthread_mutex_unlock(&mutex);
   }
 }
 
-// main = central de controle (devemos fzr assim?)
+// main = central de controle.
 int main() {
-  long sensor_id;
-
-  // instancia a side thread que cria threads de sensores.
-  pthread_create(&sensor_creator, NULL, create_sensors, NULL);
+  for (long thread = 0; thread < NUM_THREADS_SENSOR; thread++) {
+    // cria as threads eternas de sensores (vão gerar dados enquanto a main processa eles).
+    pthread_create(&threads_sensor[thread], NULL, sensor, (void *)thread);
+  }
 
   while (1) {
     // simulate processing time
     sleep(1);
 
-    // checka para resetar o sensor_id para processamento
-    if (sensor_id > NUM_THREADS_SENSOR) sensor_id = 0;
-    
-    if (sensor_data[sensor_id] == EMPTY) continue;
+    // processar com atuador (para fazer).
 
-    // isto só serve para printar e conferir se está tudo indo nos conformes.
-    for (long id = 0; id < NUM_THREADS_ATUADOR; id++) {
-      printf("(MAIN THREAD) checando array POSITION %d, VALOR: %d\n", id, sensor_data[id]);
-    }
-
-    pthread_mutex_lock(&mutex);
-    // settando data já processado para EMPTY (assim um novo sensor pode aderir novo valor)
-    printf("(MAIN THREAD) set POSITION %d, VALOR: %d para EMPTY\n", sensor_id, sensor_data[sensor_id]);
-    sensor_data[sensor_id] = EMPTY;
+    // Mutex para sincronizar
     pthread_mutex_unlock(&mutex);
 
-    // increase sensor_id para checar próximo dado de sensor.
-    sensor_id++;
+    // Checking if there are elements in the queue
+    if (sensor_queue_size > 0) {
+        // remove o primeiro elemento da esquerda e shifta os outros.
+        for (int position = 0; position < sensor_queue_size - 1; position++) {
+            sensor_queue[position] = sensor_queue[(position + 1)];
+        }
 
-    // printf("Criando atuadores (threads)...\n");
-    // create_atuadores();
+        // loop para printar.
+        printf("START\n");
+        for (int position = 0; position < sensor_queue_size - 1; position++) {
+            printf("sensor_queue[%d]: %ld \n", position, sensor_queue[position]);
+        }
+        printf("FINISH\n");
 
-    // espera todos os atuadores terminar de processar os dados (devemos fzr assim?)
-    // for (long i = 0; i < NUM_THREADS_ATUADOR; i++) {
-    //   pthread_join(threads_atuador[i], NULL);
-    // }
-    // printf("Atuadores (threads) finalizados.\n");
+        // decrementa o tamanho para salvar-mos a posi;'ao atual do array.
+        sensor_queue_size--;
+    } else {
+      printf("Array is empty, nothing to remove.\n");
+    }
+
+    pthread_mutex_unlock(&mutex);
+
   }
 
   return 0;
