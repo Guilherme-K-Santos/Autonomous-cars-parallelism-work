d64b3866d991a70c72a3efe45c7af6fc660663ed
Guilherme Focassio dos Santos <guifocassio1@gmail.com>
2024-05-11 05:27:36 -0300

    Add new function signatures for atuadores * Add functions for actuators subtasks (they're commented with the actuators code) * Rewrite comments and sensor mutex * We need to find a way to update actuators side threads with activity data and do the fork/fork-join logic

--- main.c
+++ main.c
@@ -11,21 +11,16 @@
 pthread_t threads_sensor[NUM_THREADS_SENSOR];
 pthread_t threads_atuador[NUM_THREADS_ATUADOR];
 
-// Tabela de atuadores
+// Tabela de atuadores.
 int actuators_array[NUM_THREADS_ATUADOR] = {0};
 
-// feito para controlar fluxo.
+// Mutexes
+pthread_mutex_t actuators_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t sensor_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Feito para controlar fluxo.
 long sensor_queue[ARRAY_LENGTH];
 int sensor_queue_size = 0;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-// atuadores
-void *atuador(void *arg) {
-  while (1) {
-    sleep(2);
-    // printf("simulating\n");
-  }
-}
 
 // sensores
 void *sensor(void *arg) {
@@ -42,17 +37,57 @@ void *sensor(void *arg) {
     int  random_int = rand_r(&seed) % 1000;
 
     // Mutex para sincronizar.
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&sensor_mutex);
 
     // printf("thread %ld generated %d\n", (long)arg, random_int);
 
-    // salva dados desse sensor
+    // Salva dados desse sensor.
     sensor_queue[sensor_queue_size] = random_int;
     sensor_queue_size++;
 
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&sensor_mutex);
   }
 }
+
+// Atuadores.
+void *atuador(void *arg, int activity_level) {
+  int id = *((int *)arg);
+  srand(time(NULL));
+
+  while (1) {
+    // Chama as duas subtasks de atuador (ainda precisa implementar a lógica de fork & fork-join).
+    // changeActivityLevel(id, activity_level);
+    // sendToPanel(id, activity_level);
+  }
+}
+
+// Probabilidade de falha dos atuadores (20%).
+// int hasFailed() {
+//   return rand() % 5 == 0; 
+// }
+
+// // Subtask 1 do atuador: mudar nível de atividade do atuador.
+// void changeActivityLevel(int id, int activity_level) {
+//   // Checa se a subtask vai falhar (20% de chance de falha).
+//   if (hasFailed()) {
+//     printf("Falha: Atuador %d\n", id);
+//   } else {
+//     pthread_mutex_lock(&actuators_mutex);
+//     actuators_array[id] = activity_level;
+//     pthread_mutex_unlock(&actuators_mutex);
+//   }
+// }
+
+// // Subtask 2 do atuador: mandar mensagem para o painel.
+// void sendToPanel(int id, int activity_level) {
+//   // Checa se a subtask vai falhar (20% de chance de falha).
+//   if (!hasFailed()) {
+//     printf("Alterando: Atuador %d com valor %d\n", id, activity_level);
+
+//     // Esperar um segundo para printar outra coisa.
+//     sleep(1);
+//   }
+// }
 
 // main = central de controle.
 int main() {
@@ -61,10 +96,10 @@ int main() {
     pthread_create(&threads_sensor[thread], NULL, sensor, (void *)thread);
   }
 
-  for (long thread = 0; thread < NUM_THREADS_ATUADOR; thread++) {
-    // cria as threads eternas de atuadores (vão processar os dados que a main os passar).
-    pthread_create(&threads_atuador[thread], NULL, atuador, (void *)thread);
-  }
+  // for (long thread = 0; thread < NUM_THREADS_ATUADOR; thread++) {
+  //   // cria as threads eternas de atuadores (vão processar os dados que a main os passar).
+  //   pthread_create(&threads_atuador[thread], NULL, atuador, (void *)thread);
+  // }
 
   while (1) {
     // simulate processing time
@@ -87,14 +122,14 @@ int main() {
         printf("Actuator[%d]: activity_level=%d\n", thread, actuators_array[thread]);
       }
 
-      // more things...
+      // Avise a thread responsável que o valor de atividade foi atualizado. (PARA FAZER)
 
       // remove o primeiro elemento da esquerda e shifta os outros.
       for (int position = 0; position < sensor_queue_size - 1; position++) {
         // Mutex para sincronizar
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&sensor_mutex);
         sensor_queue[position] = sensor_queue[(position + 1)];
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&sensor_mutex);
       }
 
       // loop para printar.
